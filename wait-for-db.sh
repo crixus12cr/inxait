3882cd62730ee9fd91ca4b4c7f08ceb2ff3ef689
crixus12cr <cristian2020til@gmail.com>
2025-05-17 19:20:28 -0500

    fix

--- wait-for-db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-until nc -z -v -w30 $DB_HOST $DB_PORT
-do
-  echo "Esperando a que la base de datos esté lista..."
-  sleep 5
-done
-php artisan migrate --force
-php artisan serve --host=0.0.0.0 --port=$PORT
